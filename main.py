3e455dd31a51ad4710d6549d15bbc93c1b7bd411
Aidan Border <planes4u@outlook.com>
2020-10-22 10:31:08 -0400

    Structure for main Python file, which is where the program gets ran from

---
+++ main.py
@@ -0,0 +1,16 @@
+# Aidan Border
+# CSC 1980 and CSC2280 LC3
+# Innovation Time Project -- Main File
+
+def main():
+  """
+  Main function for the program. It does all the things. Pretty kewl.
+  """
+  pass
+
+
+
+
+
+if __name__ == "__main__":
+    pass
