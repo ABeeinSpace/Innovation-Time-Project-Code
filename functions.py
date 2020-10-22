586fab6dfa06bb5c79a023694f374ca61f4474c5
Aidan Border <planes4u@outlook.com>
2020-10-22 11:19:24 -0400

    Initial commit and establishing a skeleton for the different files

---
+++ functions.py
@@ -0,0 +1,13 @@
+# Aidan Border
+# CSC 1980 and CSC2280 LC3
+# Innovation Time Project -- Helper File
+
+# This file contains all of the Fun Functions that the main.py file needs in order to do its thing.
+
+import main
+
+def printYeet():
+  """
+  docstring
+  """
+  print("double yeet.")
--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 # CSC 1980 and CSC2280 LC3
 # Innovation Time Project -- Main File
 
+import functions
+
 def main():
   """
   Main function for the program. It does all the things. Pretty kewl.
   """
-  pass
+  # print("yeet")
+  # functions.printYeet()
 
 
 
 
 
 if __name__ == "__main__":
-    pass
+    main()
